26998150b627d13e26bb85207002fbbb7df87c81
ischris74 <ischris@yahoo.com>
2020-11-12 10:10:19 -0800

    Refactored story query and pts calc for epic to its own method

--- epicrollup.groovy
+++ epicrollup.groovy
@@ -21,74 +21,84 @@ class Epic {
     Timestamp lastUpdated
 }
 
-@BaseScript CustomEndpointDelegate delegate
-getEpicRollups(httpMethod: "GET", groups: ["users"]) { MultivaluedMap queryParams, String body, HttpServletRequest request ->
+Epic getStoryPointsForEpic(String key) {
     def jqlQueryParser = ComponentAccessor.getComponent(JqlQueryParser)
     def searchService = ComponentAccessor.getComponent(SearchService.class)
     def issueManager = ComponentAccessor.getIssueManager()
     def user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
-    
-    def epics = []
-
-    Object storyPointsObj
     def storyquerystring, storyquery, storyresults
     int ptsStory
+    Object storyPointsObj
+    
+    Epic epic = new Epic()
+    epic.key = key
+    storyquerystring = "issueFunction in issuesInEpics('key = " + key + "') ORDER BY updated ASC"
+    storyquery = jqlQueryParser.parseQuery(storyquerystring)
+    storyresults = searchService.search(user, storyquery, PagerFilter.getUnlimitedFilter())
+    storyresults.getResults().each { storyIssue ->
+        storyPointsObj = storyIssue.getCustomFieldValue(ComponentAccessor.getCustomFieldManager().getCustomFieldObject(10013))
+        if (storyPointsObj instanceof Number) {
+            ptsStory = (Integer) storyPointsObj
+        } else {
+            ptsStory = 0
+        }
+        
+        if (storyIssue.getStatus().name == "Closed") {
+            epic.ptsAccepted = epic.ptsAccepted + ptsStory
+        }
+            
+        if (storyIssue.getResolution()) {
+            switch (storyIssue.getResolution().name) {
+                case "Won't Do":
+                case "Won't Fix":
+                case "Duplicate":
+                case "Incomplete":
+                case "Cannot Reproduce":
+                case "Under Advisement":
+                case "Partner Issue": 
+                    epic.ptsExcluded = epic.ptsExcluded + ptsStory
+            }
+        }
+            
+        epic.ptsTotal = epic.ptsTotal + ptsStory
+        epic.lastUpdated = storyIssue.getUpdated()
+    }
+    return epic
+}
+
+@BaseScript CustomEndpointDelegate delegate
+getEpicRollups(httpMethod: "GET", groups: ["users"]) { MultivaluedMap queryParams, String body, HttpServletRequest request ->
+    def epics = []
     
     Timestamp lastUpdated
     
     if (queryParams.getFirst("lastUpdated")) {
         lastUpdated = Timestamp.valueOf(queryParams.getFirst("lastUpdated").toString())
     }
-    
-    def epicsquerystring = "issuetype = Epic and 'Is CA PPM Task' = Yes"
-    
+       
     if (queryParams.getFirst("key")) {
-        epicsquerystring = "key = " + queryParams.getFirst("key").toString() + " and 'Is CA PPM Task' = Yes"
-    }
-    
-    def epicsquery = jqlQueryParser.parseQuery(epicsquerystring)
-    def epicsresults = searchService.search(user, epicsquery, PagerFilter.getUnlimitedFilter())
-    
-    epicsresults.getResults().each {epicIssue ->
-        Epic epic = new Epic()
-
-        storyquerystring = "issueFunction in issuesInEpics('key = " + epicIssue.key + "') ORDER BY updated ASC"
-        storyquery = jqlQueryParser.parseQuery(storyquerystring)
-        storyresults = searchService.search(user, storyquery, PagerFilter.getUnlimitedFilter())
-        epic.key = epicIssue.getKey()
-    
-        storyresults.getResults().each {storyIssue ->
-            storyPointsObj = storyIssue.getCustomFieldValue(ComponentAccessor.getCustomFieldManager().getCustomFieldObject(10013))
-            if (storyPointsObj instanceof Number) {
-                ptsStory = (Integer) storyPointsObj
-            } else {
-                ptsStory = 0
-            }
-        
-            if (storyIssue.getStatus().name == "Closed") {
-                epic.ptsAccepted = epic.ptsAccepted + ptsStory
-            }
-            
-            if (storyIssue.getResolution()){
-                switch (storyIssue.getResolution().name){
-                    case "Won't Do":
-                    case "Won't Fix":
-                    case "Duplicate":
-                    case "Incomplete":
-                    case "Cannot Reproduce":
-                    case "Under Advisement":
-                    case "Partner Issue": 
-                        epic.ptsExcluded = epic.ptsExcluded + ptsStory
-                }
-            }
-            
-            epic.ptsTotal = epic.ptsTotal + ptsStory
-            epic.lastUpdated = storyIssue.getUpdated()
-        }
+        Epic epic = getStoryPointsForEpic(queryParams.getFirst("key").toString())
         
         if (!lastUpdated || (epic.lastUpdated > lastUpdated)) {
             epics << epic
         }
-    } 
+    } else {       
+        def jqlQueryParser = ComponentAccessor.getComponent(JqlQueryParser)
+        def searchService = ComponentAccessor.getComponent(SearchService.class)
+        def issueManager = ComponentAccessor.getIssueManager()
+        def user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+        
+        def epicsquerystring = "issuetype = Epic and 'Is CA PPM Task' = Yes"
+        def epicsquery = jqlQueryParser.parseQuery(epicsquerystring)
+        def epicsresults = searchService.search(user, epicsquery, PagerFilter.getUnlimitedFilter())
+        
+        epicsresults.getResults().each { epicIssue ->
+            Epic epic = getStoryPointsForEpic(epicIssue.getKey())
+            
+            if (!lastUpdated || (epic.lastUpdated > lastUpdated)) {
+                epics << epic
+            }
+        } 
+    }
     return Response.ok(JsonOutput.toJson(epics)).build()
 }
